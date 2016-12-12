d599e286487f6f88d7159788e75c9697fa7e6020
Sarah J Spencer <sjspen@gmail.com>
2016-12-12 18:38:31 +0000

    Added function to import Qiime OTU table text files

--- spenceOTU/__init__.py
+++ spenceOTU/__init__.py
@@ -1,7 +1,7 @@
 from .reads import importFasta, removeFwdPrimer, removeFwdRevPrimer, \
 	trimLength, selectSamples, removeSamples, splitByDegenerate
 from .pairedEnds import pairConcatenate, readPairList
-from .io import exportFasta, exportPairedFasta, exportOTUtable
+from .io import exportFasta, exportPairedFasta, exportOTUtable, importQiimeOTU
 from .utilities import clusterWithUsearch
 from .otuTables import importUsearchHits, importTaxonomy, buildOTUtable, \
 	invertHits
--- spenceOTU/io.py
+++ spenceOTU/io.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 def exportFasta(reads, outFileName):
     outFile = open(outFileName, 'w')
     for read in reads:
@@ -29,3 +31,29 @@ def exportOTUtable(df, taxDict, taxType, outFileName):
         outFile.write(index + '\t')
         outFile.write('\t'.join(map(str, data.tolist())) + '\t')
         outFile.write('\t'.join(taxDict[index]) + '\n')
+
+#Include the path to a text file output from a Qiime OTU table and
+#a true/false statement indicating if there is taxonomic information
+def importQiimeOTU(textFileName, taxIncluded):
+    textFile = open(textFileName, 'r')
+    i = 0 #first line of file is a note, second line is header data
+    header = []
+    data = []
+    index = []
+    for line in textFile:
+        lineList = line.strip().split('\t')
+        if i == 1:
+            lineList = lineList[1:] #remove otu designation from first position
+            header = lineList
+        elif i > 1:
+            index.append(lineList[0])
+            lineList = lineList[1:]
+            if taxIncluded:
+                data.append(map(float, lineList[0:-1]) + [lineList[-1]])
+            else:
+                data.append(map(float, lineList))
+        i += 1
+    textFile.close()
+    df = pd.DataFrame(data, index=index, columns=header)
+    return df
+
