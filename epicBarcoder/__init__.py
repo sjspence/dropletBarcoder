4c646fbef57b3a22e0ef351308423597a5f33b6b
Manu Tamminen <mavata@gmail.com>
2017-03-01 12:55:09 +0100

    Tweaks

--- epicBarcoder/__init__.py
+++ epicBarcoder/__init__.py
@@ -3,7 +3,8 @@ from .reads import importFasta, removeFwdPrimer, removeFwdRevPrimer, \
 	splitByDegenerate
 from .pairedEnds import pairConcatenate, readPairList
 from .io import exportFasta, exportPairedFasta, exportOTUtable, importQiimeOTU
-from .utilities import clusterWithUsearch
+from .utilities import clusterWithUsearch, find_significant_connections, \
+        process_fastq_and_mapping_file, add_otus_to_fasta, move_barcodes_and_type_to_fasta_id
 from .otuTables import importUsearchHits, importTaxonomy, buildOTUtable, \
 	invertHits
 from .itol import itolHeatmap
