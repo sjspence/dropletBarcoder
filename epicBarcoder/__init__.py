29bb3b20d1dde4aadfa9272282e58d8e382dd1b1
Sarah J Spencer <sjspen@gmail.com>
2017-03-01 20:22:57 +0000

    Removed function from __init__

--- epicBarcoder/__init__.py
+++ epicBarcoder/__init__.py
@@ -5,8 +5,9 @@ from .pairedEnds import pairConcatenate, readPairList
 from .io import exportFasta, exportPairedFasta, exportOTUtable, \
 	importSintax, importQiimeOTU
 from .utilities import clusterWithUsearch, filter_significant_connections, \
-        process_fastq_and_mapping_file, add_otus_to_fasta, move_barcodes_and_type_to_fasta_id, \
+        process_fastq_and_mapping_file, add_otus_to_fasta \
         output_abunds_and_connections, output_functions
+#       move_barcodes_and_type_to_fasta_id
 from .otuTables import importUsearchHits, importTaxonomy, buildOTUtable, \
 	invertHits
 from .itol import itolHeatmap
