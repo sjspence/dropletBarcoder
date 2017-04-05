63e623fa96afab1a342825c9e142c484ef8627e9
Manu Tamminen <mavata@gmail.com>
2017-04-05 12:43:08 +0200

    Fixed import issues in init

--- epicBarcoder/__init__.py
+++ epicBarcoder/__init__.py
@@ -1,23 +1,21 @@
-from .reads import importFasta, removeFwdPrimer, \
+from .reads import importFasta, exportFasta, removeFwdPrimer, \
 	removeFwdRevPrimer, filtBarcodePrimers, trimLength, selectSamples, \
 	removeSamples, splitByDegenerate
 from .pairedEnds import pairConcatenate, readPairList
-from .io import exportFasta, exportPairedFasta, exportOTUtable, \
-	importSintax, importQiimeOTU, read_fasta, read_fastq, \
+from .io import exportPairedFasta, exportOTUtable, \
+	importQiimeOTU, read_fasta, read_fastq, \
         write_fasta, write_fastq
 from .utilities import clusterWithUsearch, filter_significant_connections, \
         process_fastq_and_mapping_file, add_otus_to_fasta, \
         output_functions, fasta_to_bc_otu_table, \
         get_grouped_table, make_otus_and_assign, \
         process_unoise_fasta, BarcodeContainer, \
-        output_abunds_and_connections, output_functions
-from .otuTables import importUsearchHits, buildOTUtable, \
-	invertHits, buildOTUtable, invertHits
+from .otuTables import buildOTUtable, invertHits
 from .usearch_io import importClusterFast
 from .itol import itolHeatmap, itolSimpleBar, itolConnections, itolHover
 from .trees import makeTreeConstraint, alignmentToSequence, tOTU_pickRepSeqs
 from .dereplicate import getUniqueSeqs, uniqueSeqsToOTU, otuToHeaders, \
-        expandDenoised, uniqueSeqsToOTU
+        expandDenoised
 from .taxonomy import importSintax, tOTUmap, importTaxonomy
 from .barcodes import createBarcodeDict, summarizeBarcoding, \
 	tOTU_singletonAbundances, tOTU_quantifyPairs, pickSigPairs
